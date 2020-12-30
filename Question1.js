596ace9d7405bf9a39a2c80450097917f2b5b646
Riya Rakshit <46257735+RakshitRiya@users.noreply.github.com>
2020-12-30 15:29:57 +0530

    Update Question1.js

--- Question1.js
+++ Question1.js
@@ -1,5 +1,9 @@
-var timestamp = parseInt(prompt("Enter number of seconds"));
-var hours = Math.floor(timestamp / 60 / 60);
-var minutes = Math.floor(timestamp / 60) - (hours * 60);
-var seconds = timestamp % 60;
+var numberofseconds = parseInt(prompt("Enter number of seconds"));
+if(isNaN(numberofseconds)){
+  document.write("please give a valid input")
+  exit();
+}
+var hours = Math.floor(numberofseconds / 60 / 60);
+var minutes = Math.floor(numberofseconds / 60) - (hours * 60);
+var seconds = numberofseconds % 60;
 document.write("Hours= "+hours+" minutes= "+minutes+" seconds= "+seconds);
